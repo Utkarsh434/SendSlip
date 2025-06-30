1c4dcca6b7413feb0c7a708df69b27bffec4d7a6
Utkarsh Prakash <114054236+Utkarsh434@users.noreply.github.com>
2025-06-30 16:36:13 +0530

    baseurlChnages_2

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -20,8 +20,8 @@ export const AppContextProvider = (props) => {
   const [invoiceTitle, setInvoiceTitle] = useState("Create Invoice");
   const [selectedTemplate, setSelectedTemplate] = useState("template1");
 
-  const baseURL = "http://localhost:8080/api";
-
+  // const baseURL = "http://localhost:8080/api";
+  const baseURL = "https://sendslipbackend-production.up.railway.app/api";
 
   const contextValue = {
     baseURL,
